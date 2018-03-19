3b0277ce045c4cc0547faf6afe770e325758283e
chunlin-li <zhglclyx@163.com>
2018-03-19 13:58:51 +0800

    consumer 中多个订阅 topic 的管理

--- README.md
+++ README.md
@@ -21,9 +21,83 @@ const Kafka = require('ease-ali-kafka');
 
 ```javascript
 Kafka.init({
-  bootstrap: 'kafka-ons-internet.aliyun.com:8080',
+  // 参考阿里云的接入点说明. https://help.aliyun.com/document_detail/52376.html
+  bootstrap: 'kafka-ons-internet.aliyun.com:8080',  
   username: '你的阿里云帐号 access key',
   password: '你的阿里云帐号 secret 后 10 位',
-  consumerID: '在阿里云上配置的 consumer ID', // 如果不需要 consumer 则不用配置该参数
+  // 如果不需要 consumer 则不用配置该参数
+  consumerID: '在阿里云上配置的 consumer ID',
+  // 发送消息后, 等待回执的超时时间, 默认 10 秒
+  reportTimeout: 3000,
+  // 自动生成 key 的编码方式, 默认 base64, 支持 base64, hex 等
+  keyEncode: 'base64',
+  // 生产者轮询本地的时间, 默认 200ms
+  producerPollInterval: 200  
 });
 ```
+
+#### 发送消息
+
+```javascript
+Kafka.sendKafkaMessage('TOPIC_NAME', {
+  id: 123,
+  message: 'hello'
+}).then(report => console.log('send success, report is ', report))
+  .catch(err => console.error('send failed. ', err));
+```
+
+**`.sendKafkaMessage(topicName, content)`**    
+
+return: `Promise<report>`
+
+`topicName`: {String} 目标topic名称    
+`content`: {String|Object|Buffer} 要发送的内容, 可以直接写 Object.    
+`report`: {Object} 消息回执. 其中主要包含字段 `key`{Buffer}, `topic`{String}, `partition`{Number}, `offset`{Number}     
+
+如因网络问题, 发送后10秒内未收到回执, 则会报 timeout 错误.
+
+  
+#### 订阅并消费消息
+
+```javascript
+Kafka.subscribe('TOPIC_NAME', function (data, commit, next) {
+  console.log('consuming data', data);
+  commit();
+  next();
+})
+```
+
+**`.subscribe(topicName, handler)`**
+
+return: `undefined`
+
+`topicName`: {String} 目标topic名称     
+`handler`: {Function|AsyncFunction} 处理消息的函数. 有多种消费方式    
+
+```javascript
+Kafka.subscribe(topic, function (data) {
+  // 可以获取到 data. 
+  // Kafka 的 commit 操作已在内部执行过了.
+  // consumer 不会等待该消息处理完毕就将继续处理下一条消息 
+});
+
+Kafka.subscribe(topic, function (data, commit) {
+  // 可以获取到 data.
+  // 可以自己通过调用 commit() 函数控制是否给 kafka 消费完成的通知
+  // commit 的同时会结束当前消息的处理, 准备处理下一条消息 
+});
+
+Kafka.subscribe(topic, function (data, commit, next) {
+  // 可以获取到 data.
+  // 可以自己通过调用 commit() 函数控制是否给 kafka 消费完成的通知
+  // 可以通过调用 next() 函数来启动下一条消息的处理, 可以自己手动控制消息的并发度 
+  // 这种模式通常用于消息处理过程中包含异步IO的时候 
+})
+```
+
+支持订阅多个不同的 topic.
+
+
+## License
+
+MIT
--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 /*
 阿里云 MQ Kafka 版的 Node.js SDK helper
-有任何问题请联系: Chunlin-Li
 */
 
 const Kafka = require('node-rdkafka');
@@ -10,14 +9,35 @@ const crypto  =require('crypto');
 const Writable = require('stream').Writable;
 
 let options = null;
-let RP_TIMEOUT = 10000; // 发送消息后, 等待回执的超时时间
 let producerWindow = {}; // 用于注册 producer 的 report 回调
-let consumers = {}; // 记录所有初始化的 consumer 实例
+let consumerInstance;
+let subscribedTopics = {};
+
+/**
+ * 初始化 kafka 配置信息
+ * @param opt.username - 阿里云公共帐号的用户名, 即帐号的 access key
+ * @param opt.password - 阿里云公共帐号的密码, 即帐号的 secret 的后10位
+ * @param opt.bootstrap - 阿里云 kafka 接入点. 请查看 https://help.aliyun.com/document_detail/52376.html
+ * @param opt.consumerID - 阿里云MQ上配置的消费者ID, 对应 Kafka 中的 groupId 的概念. 一个服务应该使用一个唯一的 ConsumerID
+ * @param opt.reportTimeout - 发送消息后, 等待回执的超时时间, 默认 10 秒
+ * @param opt.keyEncode
+ * @param opt.producerPollInterval
+ */
+exports.init = function (opt) {
+  if (options) {
+    return console.error('kafka has been initiated');
+  }
+  options = Object.assign({
+    reportTimeout: 10000,
+    keyEncode: 'base64',
+    producerPollInterval: 200
+  }, opt);
+};
 
 /**
  * 发送消息
- * @param topic - topic 名称
- * @param content - 发送内容
+ * @param topic{String} - topic 名称
+ * @param content{String|Object|Buffer} - 发送内容
  * @return Promise<Object> - Promise 的方式返回消息回执 report
  */
 exports.sendKafkaMessage = function (topic, content) {
@@ -28,10 +48,10 @@ exports.sendKafkaMessage = function (topic, content) {
       if (typeof content === 'object') {
         content = JSON.stringify(content);
       }
-      if (!(content instanceof Buffer)) {
+      if (!(content instanceof Buffer)) { // 如果不是 buffer, 则将其转为 buffer
         content = Buffer.from(content);
       }
-      let key = crypto.createHash('md5').update(content).digest('base64'); // 计算 md5 作为 key
+      let key = crypto.createHash('md5').update(content).digest(options.keyEncode); // 计算 md5 作为 key
 
       producer.produce(topic, -1, content, key); // 发送
 
@@ -39,89 +59,140 @@ exports.sendKafkaMessage = function (topic, content) {
         // 设定超时计时器
         let timeout = setTimeout(() => {
           delete producerWindow[key];
-          reject(new Error(`timeout: did not receive report in ${RP_TIMEOUT} ms`));
-        }, RP_TIMEOUT); // RP_TIMEOUT 内没有收到回执则超时
+          reject(new Error(`timeout: did not receive report in ${options.reportTimeout} ms`));
+        }, options.reportTimeout); // reportTimeout 内没有收到回执则超时
         // 注册 report 回调
         producerWindow[key] = function (report) {
-          clearTimeout(timeout); // 关闭超时计时器
+          clearTimeout(timeout); // 成功, 关闭超时计时器
           resolve(report);
         };
       })
     });
 };
 
+/**
+ * 订阅topic
+ * @param topic{String} - 要订阅的主题
+ * @param handler - 消息处理函数 function(data, commit, next) {}
+ */
 exports.subscribe = function(topic, handler) {
 
-  if (consumers[topic]) {
-    throw new Error('repeat subscribe the topic: ' + topic);
+  if (!options) {
+    throw new Error('Kafka not init, please call .init() to initiate');
   }
 
-  let stream = Kafka.KafkaConsumer.createReadStream({
-    // 'debug': 'all',
-    'api.version.request': 'true',
-    'bootstrap.servers': options.bootstrap, // 上线使用需要改为对应的阿里内网接入点
-    'security.protocol': 'sasl_ssl',
-    'ssl.ca.location': __dirname + '/ca-cert',
-    'sasl.mechanisms': 'PLAIN',
-    'sasl.username': options.username, // 公共帐号的 access key
-    'sasl.password': options.password, // 公共帐号的 secret
-    'group.id': options.consumerID,
-    'fetch.wait.max.ms': 500,
-    'enable.auto.commit': false
-  }, {}, {
-    topics: [topic]
-  });
+  let consumer = getConsumer([topic]);
 
-  consumers[topic] = stream;
+  if (subscribedTopics[topic]) {
+    throw new Error('Do not repeat subscribe topic: ' + topic);
+  }
 
-  stream.on('ready', function () {
-    console.log('consumer stream readable');
-  });
+  subscribedTopics[topic] = handler;
 
-  stream.pipe(new Writable({
-    write: function (chunk, encoding, callback) { // 实现顺序同步处理, 避免被大量消息打爆
-      if (handler.length === 1) { // 只传数据本身, 启动异步模式, 并自动完成读取和提交
-        setTimeout(handler.bind(null, chunk), 0);
-        stream.consumer.commitMessage(chunk);
-        callback();
-      } else if (handler.length === 2) { // handler 只提供了 data, commit 两参数时, 提交的同时完成读取
-        handler(chunk, function () {
-          stream.consumer.commitMessage(chunk);
-          callback();
-        });
-      } else { // 参数按照 data, commit, next 处理. 控制权交由用户
-        handler(chunk, () => stream.consumer.commitMessage(chunk), callback);
-      }
-    },
-    objectMode: true
-  }));
-
-  stream.on('error', function(err) {
-    console.error('kafka consumer stream error', err);
-    process.exit(1);
-  });
-
-  stream.consumer.on('event.error', function(err) {
-    if (err.code !== -1) {
-      console.log('event.error', err);
-    }
-  })
-
+  if (Object.keys(subscribedTopics).length > 1) { // 追加订阅的 topic, 须再执行一遍 subscribe()
+    let topics = Object.keys(subscribedTopics);
+    consumer.subscribe(topics);
+  }
 };
+
 
 /**
- * 初始化 kafka 配置信息
- * @param opt.username - 阿里云公共帐号的用户名, 即帐号的 access key
- * @param opt.password - 阿里云公共帐号的密码, 即帐号的 secret 的后10位
- * @param opt.bootstrap - 阿里云 kafka 接入点. 请查看 https://help.aliyun.com/document_detail/52376.html
- * @param opt.consumerID - 阿里云MQ上配置的消费者ID, 对应 Kafka 中的 groupId 的概念. 一个服务应该使用一个唯一的 ConsumerID
+ * 获取 producer 实例, 返回 promise
+ * @return {Promise<Object>}
  */
-exports.init = function (opt) {
-  if (options) {
-    return console.error('kafka has been initiated');
+exports.getProducer = () => getProducer.instance;
+
+/**
+ * 获取 consumer 实例. 直接返回
+ * @return {Object} consumer 实例
+ */
+exports.consumerInstance = () => consumerInstance;
+
+// 参数 topics 只在首次订阅时有效. 因为创建 ReadStream 时, 参数 topics 不能为空.
+function getConsumer (topics) {
+
+  if (!consumerInstance) {
+
+    // 初始化 consumer, stream API 方式
+    let stream = Kafka.KafkaConsumer.createReadStream({
+      // 'debug': 'all',
+      'api.version.request': 'true',
+      'bootstrap.servers': options.bootstrap, // 上线使用需要改为对应的阿里内网接入点
+      'security.protocol': 'sasl_ssl',
+      'ssl.ca.location': __dirname + '/ca-cert',
+      'sasl.mechanisms': 'PLAIN',
+      'sasl.username': options.username, // 公共帐号的 access key
+      'sasl.password': options.password, // 公共帐号的 secret
+      'group.id': options.consumerID,
+      'fetch.wait.max.ms': 500,
+      'enable.auto.commit': false
+    }, {}, {topics: topics});
+
+    stream.consumer.on('ready', function () {
+      console.log('kafka consumer connect ok');
+    });
+
+    stream.consumer.on('subscribed', function (topics) {
+      console.log('kafka consumer subscribed topics: ', topics);
+    });
+
+    stream.on('error', function (err) {
+      console.error('kafka consumer stream error', err);
+    });
+
+    stream.consumer.on('disconnected', function () {
+      console.error('kafka consumer disconnected');
+    });
+
+    stream.consumer.on('event.error', function (err) {
+      if (err.code !== -1) {
+        console.log('kafka consumer event.error', err);
+      }
+    });
+
+
+    let errorHandler = function (func) {
+      if (typeof func !== 'function') {
+        throw new Error('func must be a function');
+      }
+      if (func.constructor.name === 'AsyncFunction') { // ES7 AsyncFunction
+        func().catch(err => console.error('MQ handler error:', err));
+      } else if (func.constructor.name === 'Function') { // Function
+        try {
+          func();
+        } catch (e) {
+          console.error('MQ handler error:', e);
+        }
+      }
+    };
+
+    // 使用 stream API, 避免消费节点被打爆
+    stream.pipe(new Writable({
+      write: function (chunk, encoding, callback) { // 实现顺序同步处理, 避免被大量消息打爆
+        // 取出对应 topic 的 handler
+        let handler = subscribedTopics[chunk.topic];
+
+        if (handler.length === 1) { // 只传数据本身, 启动异步模式, 并自动完成读取和提交
+          setTimeout(errorHandler(handler.bind(null, chunk)), 0);
+          stream.consumer.commitMessage(chunk);
+          callback();
+        } else if (handler.length === 2) { // handler 只提供了 data, commit 两参数时, 提交的同时完成读取
+          errorHandler(handler.bind(null, chunk, function () {
+            stream.consumer.commitMessage(chunk);
+            callback();
+          }));
+        } else { // 参数按照 data, commit, next 处理. 控制权交由用户
+          errorHandler(handler.bind(null, chunk, () => stream.consumer.commitMessage(chunk), callback));
+        }
+      },
+      objectMode: true
+    }));
+
+    consumerInstance = stream.consumer;
   }
-  options = opt;
-};
+
+  return consumerInstance;
+}
 
 
 function getProducer () {
@@ -131,7 +202,7 @@ function getProducer () {
 
   if (!getProducer.instance) {
 
-    let producerInstance = new Kafka.Producer({
+    let producer = new Kafka.Producer({
       // 'debug': 'all',
       'api.version.request': 'true',
       'bootstrap.servers': options.bootstrap, // 上线使用需要改为对应的阿里内网接入点
@@ -144,35 +215,35 @@ function getProducer () {
       'sasl.password': options.password, // 公共帐号的 secret
     });
     let ready = new Promise((resolve, reject) => {
-      producerInstance.on('ready', function () {
-        console.log('kafka producer connect ok', new Date());
-        resolve(producerInstance);
+      producer.on('ready', function () {
+        console.log('kafka producer connect ok');
+        resolve(producer);
       });
-      producerInstance.on('error', function (error) {
+      producer.on('error', function (error) {
         console.error('error:', error);
         reject(error);
       });
     });
 
-    producerInstance.on('disconnected', function () {
+    producer.on('disconnected', function () {
       //断线自动重连
       console.error('disconnected');
-      producerInstance.connect();
+      producer.connect();
     });
-    producerInstance.on('event.error', function (err) {
+    producer.on('event.error', function (err) {
       //AliKafka服务器会主动掐掉空闲连接，如果发现这个异常，则客户端重连(先disconnect再connect)
       if (-1 !== err.code) {
         console.error(new Date(), 'event.error:', err);
       }
     });
-    producerInstance.on('delivery-report', function(err, report) {
+    producer.on('delivery-report', function(err, report) {
       //消息发送成功，这里会收到report
       let reportCallback = producerWindow[report.key.toString()];
       reportCallback && reportCallback(report);
     });
 
-    producerInstance.setPollInterval(200);
-    producerInstance.connect();
+    producer.setPollInterval(options.producerPollInterval);
+    producer.connect();
     getProducer.instance = ready;
   }
 
